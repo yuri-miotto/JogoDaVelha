acf09868b1f4dd0c44845d1f6d0db395121d4c0a
yuri-miotto <yurimagrinimiotto@gmail.com>
2023-03-12 22:21:21 -0300

    adicionado verificação se a posição já foi escolhida

--- JogoDaVelha/Program.cs
+++ JogoDaVelha/Program.cs
@@ -4,6 +4,15 @@
     {
         int contador = 0;
         char[,] tabuleiro = new char[3, 3];
+        int posicao1 = 0;
+        int posicao2 = 0;
+        int posicao3 = 0;
+        int posicao4 = 0;
+        int posicao5 = 0;
+        int posicao6 = 0;
+        int posicao7 = 0;
+        int posicao8 = 0;
+        int posicao9 = 0;
 
         tabuleiro[0, 0] = '1';
         tabuleiro[0, 1] = '2';
@@ -39,48 +48,143 @@
             char posicao = char.Parse(Console.ReadLine());
 
             switch (posicao)
-            {
-                case '1':
-                    tabuleiro[0, 0] = 'X';
+                {
+                    case '1':
+                    if(posicao1 == 0)
+                    {
+                        tabuleiro[0, 0] = 'X';
+                        posicao1++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador1();
+                    }
                     break;
 
-                case '2':
-                    tabuleiro[0, 1] = 'X';
+                    case '2':
+                    if(posicao2 == 0)
+                    {
+                        tabuleiro[0, 1] = 'X';
+                        posicao2++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador1();
+                    }
                     break;
 
-                case '3':
-                    tabuleiro[0, 2] = 'X';
+                    case '3':
+                    if(posicao3 == 0)
+                    {
+                        tabuleiro[0, 2] = 'X';
+                        posicao3++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador1();
+                    }
                     break;
 
-                case '4':
-                    tabuleiro[1, 0] = 'X';
+                    case '4':
+                    if(posicao4 == 0)
+                    {
+                        tabuleiro[1, 0] = 'X';
+                        posicao4++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador1();
+                    }
                     break;
 
-                case '5':
-                    tabuleiro[1, 1] = 'X';
+                    case '5':
+                    if(posicao5 == 0)
+                    {
+                        tabuleiro[1, 1] = 'X';
+                        posicao5++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador1();
+                    }
                     break;
 
-                case '6':
-                    tabuleiro[1, 2] = 'X';
+                    case '6':
+                    if(posicao6 == 0)
+                    {
+                        tabuleiro[1, 2] = 'X';
+                        posicao6++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador1();
+                    }
                     break;
 
-                case '7':
-                    tabuleiro[2, 0] = 'X';
+                    case '7':
+                    if(posicao7 == 0)
+                    {
+                        tabuleiro[2, 0] = 'X';
+                        posicao7++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador1();
+                    }
                     break;
 
-                case '8':
-                    tabuleiro[2, 1] = 'X';
+                    case '8':
+                    if(posicao8 == 0)
+                    {
+                        tabuleiro[2, 1] = 'X';
+                        posicao8++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador1();
+                    }
                     break;
 
-                case '9':
-                    tabuleiro[2, 2] = 'X';
+                    case '9':
+                    if(posicao9 == 0)
+                    {
+                        tabuleiro[2, 2] = 'X';
+                        posicao9++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador1();
+                    }
                     break;
 
-                default:
-                    Console.WriteLine("Opção inválida");
-                    PosicaoJogador1();
-                    break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        Console.WriteLine();
+                        PosicaoJogador1();
+                        break;
             }
+
+            
+
+            
             VerificaVencedor1();
         }
 
@@ -93,46 +197,139 @@
             switch (posicao)
             {
                 case '1':
-                    tabuleiro[0, 0] = 'O';
+                    if (posicao1 == 0)
+                    {
+                        tabuleiro[0, 0] = 'O';
+                        posicao1++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador2();
+                    }
                     break;
 
                 case '2':
-                    tabuleiro[0, 1] = 'O';
+                    if (posicao2 == 0)
+                    {
+                        tabuleiro[0, 1] = 'O';
+                        posicao2++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador2();
+                    }
                     break;
 
                 case '3':
-                    tabuleiro[0, 2] = 'O';
+                    if (posicao3 == 0)
+                    {
+                        tabuleiro[0, 2] = 'O';
+                        posicao3++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador2();
+                    }
                     break;
 
                 case '4':
-                    tabuleiro[1, 0] = 'O';
+                    if (posicao4 == 0)
+                    {
+                        tabuleiro[1, 0] = 'O';
+                        posicao4++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador2();
+                    }
                     break;
 
                 case '5':
-                    tabuleiro[1, 1] = 'O';
+                    if (posicao5 == 0)
+                    {
+                        tabuleiro[1, 1] = 'O';
+                        posicao5++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador2();
+                    }
                     break;
 
                 case '6':
-                    tabuleiro[1, 2] = 'O';
+                    if (posicao6 == 0)
+                    {
+                        tabuleiro[1, 2] = 'O';
+                        posicao6++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador2();
+                    }
                     break;
 
                 case '7':
-                    tabuleiro[2, 0] = 'O';
+                    if (posicao7 == 0)
+                    {
+                        tabuleiro[2, 0] = 'O';
+                        posicao7++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador2();
+                    }
                     break;
 
                 case '8':
-                    tabuleiro[2, 1] = 'O';
+                    if (posicao8 == 0)
+                    {
+                        tabuleiro[2, 1] = 'O';
+                        posicao8++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador2();
+                    }
                     break;
 
                 case '9':
-                    tabuleiro[2, 2] = 'O';
+                    if (posicao9 == 0)
+                    {
+                        tabuleiro[2, 2] = 'O';
+                        posicao9++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Essa posição já foi escolhida!");
+                        Console.WriteLine();
+                        PosicaoJogador2();
+                    }
                     break;
 
                 default:
                     Console.WriteLine("Opção inválida");
+                    Console.WriteLine();
                     PosicaoJogador2();
                     break;
             }
+
+
             VerificaVencedor2();
         }
 
@@ -151,7 +348,8 @@
 
             {
                 Console.WriteLine("Jogador 1 ganhou!!!");
-                Console.WriteLine("Fim do jogo.");
+                Console.WriteLine();
+                FimDoJogo();
             }
             else
             {
@@ -159,6 +357,7 @@
                 if (contador == 9)
                 {
                     Console.WriteLine("Deu velha!");
+                    Console.WriteLine();
                     FimDoJogo();
                 }
                 else
@@ -186,7 +385,8 @@
 
             {
                 Console.WriteLine("Jogador 2 ganhou!!!");
-                Console.WriteLine("Fim do jogo.");
+                Console.WriteLine();
+                FimDoJogo();
 
             }
             else
@@ -195,6 +395,7 @@
                 if (contador == 9)
                 {
                     Console.WriteLine("Deu velha!");
+                    Console.WriteLine();
                     FimDoJogo();
                 }
                 else
